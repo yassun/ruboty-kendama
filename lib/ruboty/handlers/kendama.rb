5a1fb5953abc5065e48afbc19a6b95a45ddf3f2f
Taga Yasunori <yasukun.taga@gmail.com>
2015-04-19 13:54:20 +0900

    Add handlers

--- lib/ruboty/handlers/kendama.rb
+++ lib/ruboty/handlers/kendama.rb
@@ -3,11 +3,20 @@ require "ruboty/kendama/actions/kendama"
 module Ruboty
   module Handlers
     class Kendama < Base
-      on /kendama kendama/, name: 'kendama', description: 'TODO: write your description'
+      on /kendama/, name: 'kendama', description: 'Search kendama trick from youtube.'
 
       def kendama(message)
-        Ruboty::Kendama::Actions::Kendama.new(message).call
+        if url = search
+          message.reply(url)
+        end
       end
+
+      private
+
+      def search
+        Ruboty::Kendama::Actions::Kendama.new.get
+      end
+
     end
   end
 end
